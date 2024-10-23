73e1f2fdaf335291ebf135999f166dee2fe2a9c8
ohayer <olik03.2003@wp.pl>
2024-10-23 19:13:44 +0200

    Build project

---
+++ backend/src/main/java/pl/currencyinfo/curinf/CurinfApplication.java
@@ -0,0 +1,13 @@
+package pl.currencyinfo.curinf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurinfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurinfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/currencyinfo/curinf/CurinfApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.currencyinfo.curinf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurinfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
